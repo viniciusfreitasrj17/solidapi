15f3330994cbb0da5e794a9c0897e5f5e43820be
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-09-12 20:55:52 -0300

    Add await in method sendMail

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -29,7 +29,7 @@ export class CreateUserUseCase {
 
     await this.usersRepository.save(user);
 
-    this.mailProvider.sendMail({
+    await this.mailProvider.sendMail({
       to: {
         name: data.name,
         email: data.email,
